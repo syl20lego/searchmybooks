9ab56d4bf7f9696e2a935f610c79f75a7bfe906b
Sylvain Legault <Sylvain.Legault@pj.ca>
2016-01-03 17:15:21 -0500

    added infinite scroll, and pages structures

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -1,22 +1,70 @@
 (function(){
-    var app = angular.module('books',  ['ngRoute', 'angularFileUpload']);
+    var app = angular.module('books',  ['ngRoute', 'angularFileUpload', 'infinite-scroll']);
+
+    // Application configuration:
 
     app.config(function( $locationProvider){
 
     });
 
+    // Define routes
     app.config(function($routeProvider){
         $routeProvider
             // route for the home page
             .when('/', {
-                templateUrl : 'pages/books.html',
-                controller  : 'mainController'
+                templateUrl : 'pages/default.html',
+                controller  : 'searchController'
+            })
+            .when('/search', {
+                templateUrl : 'pages/search.html',
+                controller  : 'searchController'
+            })
+            .when('/upload', {
+                templateUrl : 'pages/upload.html',
+                controller  : 'uploadController'
+            })
+            .when('/admin', {
+                templateUrl : 'pages/admin.html',
+                controller  : 'adminController'
             });
     });
 
+    // Application  Directives:
 
+    // Set active class to currently selected menu item by comparing path
+    app.directive('isActiveNav', [ '$location', function($location) {
+        return {
+            restrict: 'A',
+            link: function(scope, element) {
+                scope.location = $location;
+                scope.$watch('location.path()', function(currentPath) {
+                    if('/#' + currentPath === element[0].attributes['href'].nodeValue) {
+                        element.parent().addClass('active');
+                    } else {
+                        element.parent().removeClass('active');
+                    }
+                });
+            }
+        };
+    }]);
 
-    app.controller('mainController', ['$scope', 'FileUploader', function($scope, FileUploader) {
+    app.controller('searchController', function($scope, $http) {
+        $scope.formData = {};
+
+        $scope.pagingFunction = function() {
+            console.log('Paging');
+            $http.get('/books/')
+                .success(function (data) {
+                    $scope.data = data;
+                    console.log('reloaded : ' + $scope.data);
+                })
+                .error(function (data) {
+                    console.log('Error: ' + data);
+                });
+        };
+    });
+
+    app.controller('uploadController', ['$scope', 'FileUploader', function($scope, FileUploader) {
         var uploader = $scope.uploader = new FileUploader({
             url: '/books/upload'
         });
@@ -24,12 +72,24 @@
         // FILTERS
 
         uploader.filters.push({
-            name: 'customFilter',
+            name: 'maxQueue',
             fn: function(item /*{File|FileLikeObject}*/, options) {
-                return this.queue.length < 10;
+                return this.queue.length < 2;
+            }
+        });
+        uploader.filters.push({
+            name: 'extensionName',
+            fn: function(item /*{File|FileLikeObject}*/, options) {
+                return item.type === 'application/pdf';
             }
         });
 
+        uploader.filters.push({
+            name: 'disabledElement',
+            fn: function(item /*{File|FileLikeObject}*/, options) {
+                return !options.disabled;
+            }
+        });
         // CALLBACKS
 
         uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
@@ -68,4 +128,8 @@
 
         console.info('uploader', uploader);
     }]);
+
+    app.controller('adminController', function(){
+
+    });
 })();
